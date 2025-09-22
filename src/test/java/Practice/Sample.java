94f128226359ddfc2d1cf41d229fe1345d5437ae
Manoj2204 <mahtomanojkumar824@gmail.com>
2025-09-22 17:02:34 +0530

    This is my first commit

---
+++ src/test/java/Practice/Sample.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+		
+	    @Test
+		public void Demo2()
+		{
+		   System.out.println("this is new branch ATE");
+		}
+		
+	}
+
+
